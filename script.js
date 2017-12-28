8a49ab5b2de8c1f9f33d34d49c31d8c68f33c110
spieszku <spiechowicz.a@gmail.com>
2017-12-28 14:47:38 +0100

    Add all movies counter, seen movies counter, updating symbol and counter

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+let moviesCounterAll = moviesData.length;
+let moviesCounterSeen = 0;
+for (let [index, movieItem] of moviesData.entries()) {
+    let movieStatus;
+    //count seen movies
+    if (movieItem.seen === "T") {
+        moviesCounterSeen++;
+        movieStatus = "V";
+    }
+    else {
+        movieStatus = "X"
+    }
 
+    //create movieList element from moviesData
+    let listedMovie = document.createElement("LI");
+    listedMovie.innerHTML =
+        "<p>" + movieItem.title + "</p>" +
+        "<p>" + movieItem.year + "</p>" +
+        "<p>" + movieItem.summary + "</p>" +
+        "<p>" + movieItem.genre + "</p>" +
+        "<p id='movie-" + index + "' onclick='updateMovies(" + index + ")'>" + movieStatus + "</p>";
+    document.querySelector("#moviesList").appendChild(listedMovie);
+}
+
+//display number of all movies
+document.querySelector("#moviesCounterAll").innerHTML = moviesCounterAll;
+//display number of seen movies
+document.querySelector("#moviesCounterSeen").innerHTML = moviesCounterSeen;
+
+//update seen movies counter and symbol
+function updateMovies(index) {
+    let activeMovie = moviesData[index];
+    let movieStatus;
+    if (activeMovie.seen === "F") {
+        activeMovie.seen = "T";
+        moviesCounterSeen++;
+        movieStatus = "V"
+    }
+    else if (activeMovie.seen === "T") {
+        activeMovie.seen = "F";
+        moviesCounterSeen--;
+        movieStatus = "X";
+    }
+    document.querySelector("#moviesCounterSeen").innerHTML = moviesCounterSeen;
+    document.querySelector("#movie-" + index).innerHTML = movieStatus;
+}
