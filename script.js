967515876ffd0cd859cb178cf00c8fdff8ecf109
spieszku <spiechowicz.a@gmail.com>
2017-12-30 19:25:03 +0100

    Add symbols of seen/unseen movies and refactoring

--- script.js
+++ script.js
@@ -1,46 +1,54 @@
+const symbolTrue = "&#x2611";
+const symbolFalse = "&#x2612";
 let moviesCounterAll = moviesData.length;
 let moviesCounterSeen = 0;
+
+// display number of all movies
+document.querySelector("#moviesCounterAll").innerHTML = moviesCounterAll;
+
+// update displayed value of movies counter seen and movie status
+function updateSeenCounter(index, moviesCounterSeen, movieStatus) {
+    // display number of seen movies
+    document.querySelector("#moviesCounterSeen").innerHTML = moviesCounterSeen;
+    document.querySelector("#movie-" + index).innerHTML = "Seen: " + movieStatus;
+}
+
 for (let [index, movieItem] of moviesData.entries()) {
     let movieStatus;
-    //count seen movies
+    // count seen movies
     if (movieItem.seen === "T") {
         moviesCounterSeen++;
-        movieStatus = "V";
+        movieStatus = symbolTrue;
     }
     else {
-        movieStatus = "X"
+        movieStatus = symbolFalse;
     }
 
-    //create movieList element from moviesData
+    // create movieList element from moviesData
     let listedMovie = document.createElement("LI");
     listedMovie.innerHTML =
         "<p>" + movieItem.title + "</p>" +
         "<p class='col-3'>Year: " + movieItem.year + "</p>" +
         "<p class='col-3'>Genre: " + movieItem.genre + "</p>" +
-        "<p class='col-3 movie-status' id='movie-" + index + "' onclick='updateMovies(" + index + ")'>Seen: " + movieStatus + "</p>" +
+        "<p class='col-3 movie-status' id='movie-" + index + "' onclick='updateMovies(" + index + ")'></p>" +
         "<p>" + movieItem.summary + "</p>";
     document.querySelector("#moviesList").appendChild(listedMovie);
+    updateSeenCounter(index, moviesCounterSeen, movieStatus);
 }
 
-//display number of all movies
-document.querySelector("#moviesCounterAll").innerHTML = moviesCounterAll;
-//display number of seen movies
-document.querySelector("#moviesCounterSeen").innerHTML = moviesCounterSeen;
-
-//update seen movies counter and symbol
+// update seen movies counter and symbol
 function updateMovies(index) {
     let activeMovie = moviesData[index];
     let movieStatus;
     if (activeMovie.seen === "F") {
         activeMovie.seen = "T";
         moviesCounterSeen++;
-        movieStatus = "V"
+        movieStatus = symbolTrue;
     }
-    else if (activeMovie.seen === "T") {
+    else {
         activeMovie.seen = "F";
         moviesCounterSeen--;
-        movieStatus = "X";
+        movieStatus = symbolFalse;
     }
-    document.querySelector("#moviesCounterSeen").innerHTML = moviesCounterSeen;
-    document.querySelector("#movie-" + index).innerHTML = "Seen: " + movieStatus;
+    updateSeenCounter(index, moviesCounterSeen, movieStatus);
 }
